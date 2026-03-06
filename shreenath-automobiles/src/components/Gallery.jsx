9cd08d125085a93957e886056eb596b545275b5b
kshitij17kc <kshitij7667@gmail.com>
2026-03-06 15:57:01 +0530

    Update Gallery.jsx

--- shreenath-automobiles/src/components/Gallery.jsx
+++ shreenath-automobiles/src/components/Gallery.jsx
@@ -10,7 +10,7 @@ export default function Gallery() {
           <img src="/images/logo.png" alt="Workshop logo" className="rounded-xl" />
           <img src="/images/hero-car.png" alt="Car service" className="rounded-xl" />
           <img src="/images/service-1.jpg" alt="Vehicle repair service" className="rounded-xl" />
-          <img src="/images/service-2.jpg" alt="Automobiles maintenance" className="rounded-xl" />
+          <img src="/images/service-2.jpg" alt="Automobile maintenance" className="rounded-xl" />
         </div>
       </div>
     </section>
